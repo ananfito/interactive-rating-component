b82062c2a6730779825520d07ced877e52efe8f3
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-06-03 10:53:07 +0700

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,22 @@ function App() {
 
   return (
     <div className="form-container">
-      <h1>This is the header</h1>
-
+      <img id='star-icon' src="./assets/icon-star.svg" alt="star icon" width='10px' height='10px' />
+      <h1 className='form-container--title'>How'd we do?</h1>
+      <p className='form-container--text'>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+      <form action="" className='form-container--form'>
+        <input type="radio" name="rating-one" id="rating-one" value={1} />
+        <label className='form--label' htmlFor="rating-one">1</label>
+        <input type="radio" name="rating-two" id="rating-two" value={2} />
+        <label className='form--label' htmlFor="rating-two">2</label>
+        <input type="radio" name="rating-three" id="rating-three" value={3} />
+        <label className='form--label' htmlFor="rating-three">3</label>
+        <input type="radio" name="rating-four" id="rating-four" value={4} />
+        <label className='form--label' htmlFor="rating-four">4</label>
+        <input type="radio" name="rating-five" id="rating-five" value={5} />
+        <label className='form--label' htmlFor="rating-five">5</label>
+        <button className='form-btn'>Submit</button>
+      </form>
     </div>
   )
 }
